60811324db83abf2ec18fd7b65dfbec4f9becebe
stsupertam <supmantam@gmail.com>
2017-02-12 02:52:44 +0700

    add file after remove ignorefile

---
+++ Project Spaceshooter/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour {
+    private float height;
+    private float width;
+
+    public float [] get_screen(){
+        Debug.Log("Hello world");
+        float [] screen = new float[2];
+        screen[0] = height;
+        screen[1] = width;
+        return screen;
+    }
+
+    void Start () {
+        float distance = Camera.main.transform.position.y - this.transform.position.y;
+        height = 2.0f * Mathf.Tan(0.5f * Camera.main.fieldOfView * Mathf.Deg2Rad) * distance;
+        width = height * Screen.width / Screen.height;
+        this.transform.localScale = new Vector3(height, 1.0f, width);
+    }
+}
---
+++ Project Switch/Assets/Script/LightController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public GameObject LightObject;
+    public bool LightStatus;
+    // Use this for initialization
+    void Start()
+    {
+		
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Time.time);
+    }
+    void OnGUI()
+    {
+
+        if (LightStatus == true)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 100), "Turn OFF"))
+            {
+                Debug.Log(Time.time + "Hello World");
+                LightStatus = false;
+                LightObject.GetComponent<Light>().enabled = false;
+            }
+        }
+        else
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 100), "Turn ON"))
+            {
+                Debug.Log(Time.time + "Hello World");
+                LightStatus = true;
+                LightObject.GetComponent<Light>().enabled = true;
+            }
+        }
+    }
+}
