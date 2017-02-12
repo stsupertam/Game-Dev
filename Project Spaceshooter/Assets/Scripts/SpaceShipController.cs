e70c2e2f7a7acd32525017b8d2b1f2d12c0d9071
stsupertam <supmantam@gmail.com>
2017-02-13 01:04:36 +0700

    add rotate spaceship according mouse cursor position

--- Project Spaceshooter/Assets/Scripts/SpaceShipController.cs
+++ Project Spaceshooter/Assets/Scripts/SpaceShipController.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class SpaceShipController : MonoBehaviour{
 
-    // Use this for initialization
     public float speed;
     public GameObject myBullet;
     public GameObject plane;
     private float [] screen;
     private float xMin, xMax, zMin, zMax;
+
     void Start(){
         screen = plane.GetComponent<PlaneController>().get_screen();
         xMin = -screen[1]/2.0f;
         xMax = screen[1]/2.0f;
         zMin = -screen[0]/2.0f;
         zMax = screen[0]/2.0f;
+    }
+
+    void rotate(){
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, -angle + 180, 0));
     }
 
     void movement(){
@@ -32,8 +41,9 @@ public class SpaceShipController : MonoBehaviour{
     }
 
     void Update(){
+        rotate();
         movement();
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
             Instantiate (myBullet,this.gameObject.transform.position,Quaternion.identity);
         }
     }
